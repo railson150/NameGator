57b103974332b11dece36e8a0b107d57b8e28e27
railson150 <railsontutoriais@gmail.com>
2020-04-09 02:32:15 -0300

    FrontEnd Da Aplicação

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NomeGator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de Nomes utilizando Vue.js, GraphQl e Node</h6>
+    </div>
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
+
+export default {
+  name: "App",
+  components: {
+    DomainList
+  }
+};
+</script>
+
+<style>
+#slogan {
+  margin: 30px 0px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding: 30px 0px;
+}
+</style>
